81c99b14370e00d7b4bcf532fcc02b7e791a28bb
Icheng Lin <41904540+ichenglin@users.noreply.github.com>
2026-09-12 23:10:21 -0500

    Added Report Uncertainty

--- modules/report.py
+++ modules/report.py
@@ -12,13 +12,16 @@ def report_samples(samples: list[Station], key: str = "signal", reverse: bool = 
         f" | {sample.bss}"
     ]) for index, sample in enumerate(ordered)]))
 
-def report_distance(actual: Coordinate, guess: ProximateCoordinate, distance: int) -> None:
+def report_distance(actual: Coordinate, guess: ProximateCoordinate, distance: float) -> None:
     print("".join([
-        f"Guess:    {guess.latitude:<9}"   [:(9 + 10)],
-        f", {guess.longitude:<9}"          [:(9 + 3)],
-        f"\nActual:   {actual.latitude:<9}"[:(9 + 11)],
-        f", {actual.longitude:<9}"         [:(9 + 3)],
-        f"\nDistance: {int(distance)} meter(s)"
+        f"Guess:       {guess.latitude:<9}"   [:(9 + 13)],
+        f", {guess.longitude:<9}"             [:(9 + 3 )],
+        f"\nActual:      {actual.latitude:<9}"[:(9 + 14)],
+        f", {actual.longitude:<9}"            [:(9 + 3 )],
+        f"\nUncertainty: {guess.accuracy:<9}" [:(9 + 14)],
+         " meter(s)",
+        f"\nDistance:    {distance:<9}"       [:(9 + 14)],
+         " meter(s)",
     ]))
 
 def report_border() -> None:
